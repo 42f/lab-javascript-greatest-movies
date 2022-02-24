c05cc4a1b4cca5b70d55ca1950d39b126292f9df
bvalette <bvalette@student.42.fr>
2022-02-24 17:05:21 +0100

    done

--- src/movies.js
+++ src/movies.js
@@ -1,28 +1,129 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(movies) {
+  return movies.map((movie) => movie.director);
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(movies) {
+  return movies.filter((movie) =>
+    movie.director === 'Steven Spielberg' && movie.genre.includes('Drama'))
+    .length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(movies) {
+  let output = 0;
+  if (movies.length > 0) {
+    output = movies.reduce((prev, curr) => prev + (curr.score || 0), 0);
+    output /= movies.length;
+  }
+  return Math.round(output * 100) / 100;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore() {}
+function dramaMoviesScore(movies) {
+  return scoresAverage(movies.filter((movie) => movie.genre.includes('Drama')));
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear() {}
+function orderByYear(movies) {
+  const sortedMovies = [...movies];
+  sortedMovies.sort((a, b) => {
+    if (a.year === b.year && b.title < a.title) {
+      return 1;
+    } else if (a.year === b.year && b.title > a.title) {
+      return -1;
+    }
+    return a.year - b.year;
+  })
+  return sortedMovies;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically() {}
+function moviesCmp(a, b) {
+  if (a.title < b.title) {
+    return -1;
+  } else if (a.title > b.title) {
+    return 1;
+  }
+  return 0;
+}
+
+function scoreCmp(a, b) {
+  return a.score - b.score;
+}
+
+function orderAlphabetically(movies) {
+  const sortedMovies = [...movies];
+  return sortedMovies
+    .sort((a, b) => moviesCmp(a, b))
+    .slice(0, 20)
+    .sort((a, b) => scoreCmp(a, b))
+    .map((movie) => movie.title);
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() {}
+
+function convertToMinutes(arr) {
+  return parseInt(arr[0] || 0) * 60 + parseInt(arr[2] || 0);
+}
+
+function turnHoursToMinutes(movies) {
+
+  const moviesDeepCopy = JSON.parse(JSON.stringify(movies))
+
+  return moviesDeepCopy.map(movie => {
+    if (movie.duration && movie.duration.length) {
+      const numValues = movie.duration.split(/[^0-9]/);
+      movie.duration = convertToMinutes(numValues);
+    }
+    return movie;
+  });
+}
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() {}
+
+function getMinMaxYear(movies) {
+  return {
+    minYear: movies[0].year || 0,
+    maxYear: movies[movies.length - 1].year || 0
+  }
+}
+
+function getBestYear(years) {
+  let bestYear = {};
+  for (let year in years) {
+    let yearAv = scoresAverage(years[year]);
+    if (!bestYear.averageScore || yearAv > bestYear.averageScore) {
+      bestYear.year = year;
+      bestYear.averageScore = yearAv;
+    }
+  }
+  return bestYear;
+}
+
+function getMoviesByYear(movies) {
+  let { minYear, maxYear } = getMinMaxYear(movies);
+
+  const years = {};
+  for (let i = 0; minYear <= maxYear; minYear++, i++) {
+    years[minYear] = movies.filter(m => m.year === minYear);
+  }
+  return years;
+}
+
+function bestYearAvg(moviesOriginal) {
+  if (moviesOriginal.length) {
+    const sortedMovies = orderByYear(JSON.parse(JSON.stringify(moviesOriginal)));
+    const sortedYear = getMoviesByYear(sortedMovies);
+    const bestYear = getBestYear(sortedYear);
+
+    return `The best year was ${bestYear.year} with an average score of ${bestYear.averageScore}`;
+  }
+  return null;
+}
 
 
 
